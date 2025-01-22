680e97a21ffd3f9d66a8e390bb921a7b8e8b6453
spitfire096 <domeji14@gmail.com>
2025-01-22 10:24:54 -0500

    modified userdata

--- userdata/backend.sh
+++ userdata/backend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DATABASE_PASS='admin123'
+DATABASE_PASS='<your password>'
 
 # MEmcache
 yum install epel-release -y
@@ -36,6 +36,7 @@ systemctl start mariadb
 systemctl enable mariadb
 
 #restore the dump file for the application
+# update your password in field 
 mysqladmin -u root password "$DATABASE_PASS"
 mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
 mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
@@ -43,8 +44,8 @@ mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
 mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
 mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
 mysql -u root -p"$DATABASE_PASS" -e "create database accounts"
-mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'localhost' identified by 'admin123'"
-mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'app01' identified by 'admin123'"
+mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'localhost' identified by '<your password>'"
+mysql -u root -p"$DATABASE_PASS" -e "grant all privileges on accounts.* TO 'admin'@'app01' identified by '<your password>'"
 mysql -u root -p"$DATABASE_PASS" accounts < /vagrant/vprofile-repo/src/main/resources/db_backup.sql
 mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
 
